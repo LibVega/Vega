cdf52ae380752013710622b956a2db30a9f32b13
Sean Moss <moss.seank@gmail.com>
2020-08-28 11:33:19 -0600

    Coroutine base class and internal implementations

---
+++ Vega/Core/Coroutine/Coroutine.Impl.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Microsoft Public License (Ms-PL) - Copyright (c) 2020 Sean Moss
+ * This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
+ * the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Vega
+{
+	// Internal implementation for IEnumerator based coroutines
+	internal class EnumeratorCoroutine : Coroutine
+	{
+		private readonly IEnumerator<object?> _ticker;
+		private readonly bool _unscaled;
+		public override bool UseUnscaledTime => _unscaled;
+
+		public EnumeratorCoroutine(IEnumerator<object?> ticker, bool unscaled)
+		{
+			_ticker = ticker;
+			_unscaled = unscaled;
+		}
+
+		protected internal override object? Tick()
+		{
+			return _ticker.MoveNext() ? _ticker.Current : END;
+		}
+	}
+
+	// Internal implementation for delayed and repeating action time-based coroutines
+	internal class TimerCoroutine : EnumeratorCoroutine
+	{
+		public readonly Func<bool> Action;
+		public readonly float Delay;
+		public readonly bool Repeat;
+
+		public TimerCoroutine(float delay, bool repeat, bool unscaled, Func<bool> action)
+			: base(timer_func(delay, repeat, action), unscaled)
+		{
+			Action = action;
+			Delay = delay;
+			Repeat = repeat;
+		}
+
+		private	static IEnumerator<object?> timer_func(float delay, bool repeat, Func<bool> action)
+		{
+			do {
+				yield return WaitForSeconds(delay);
+			}
+			while (action() && repeat);
+		}
+	}
+}
---
+++ Vega/Core/Coroutine/Coroutine.cs
@@ -0,0 +1,129 @@
+﻿/*
+ * Microsoft Public License (Ms-PL) - Copyright (c) 2020 Sean Moss
+ * This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
+ * the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
+ */
+
+using System;
+
+namespace Vega
+{
+	/// <summary>
+	/// Coroutine execution policies to control when coroutines are ticked.
+	/// </summary>
+	public enum CoroutinePolicy
+	{
+		/// <summary>
+		/// The coroutine is ticked during the call to <see cref="Core.BeginFrame"/>.
+		/// </summary>
+		Beginning,
+		/// <summary>
+		/// The coroutine is ticked during the call to <see cref="Core.EndFrame"/>.
+		/// </summary>
+		End
+	}
+
+	/// <summary>
+	/// Implements functionality for delayed and spread-out task execution using enumerators.
+	/// </summary>
+	public abstract class Coroutine
+	{
+		/// <summary>
+		/// Special return value for <see cref="Tick"/> to signal the coroutine has completed execution.
+		/// </summary>
+		protected internal static readonly object END = new object();
+
+		#region Fields
+		// Record of wait object implementation
+		internal WaitObjects WaitImpl = new WaitObjects(0, null);
+
+		/// <summary>
+		/// The number of times that this coroutine has been ticked.
+		/// </summary>
+		public uint TickCount { get; internal set; } = 0;
+
+		/// <summary>
+		/// If the coroutine is currently running (being actively ticked).
+		/// </summary>
+		public bool Running { get; internal set; } = false;
+
+		/// <summary>
+		/// If the coroutine uses unscaled (<see cref="AppTime.RealDelta"/>) time for time-based pauses. Defaults to
+		/// <see langword="false"/>.
+		/// </summary>
+		public virtual bool UseUnscaledTime => false;
+
+		/// <summary>
+		/// The execution policy for the coroutine. Defaults to <see cref="CoroutinePolicy.Beginning"/>.
+		/// </summary>
+		public virtual CoroutinePolicy Policy => CoroutinePolicy.Beginning;
+		#endregion // Fields
+
+		#region Waiting
+		// Implementation of waitable objects on coroutine
+		internal record WaitObjects(float Time, Coroutine? Coroutine);
+		// Implementation of return value for time-waiting coroutines
+		internal record WaitForTimeImpl(float Seconds);
+
+		/// <summary>
+		/// Returns an object compatible with the <see cref="Tick"/> return value to cause the coroutine to wait for
+		/// the given number of seconds.
+		/// </summary>
+		/// <param name="seconds">The number of seconds to wait the coroutine for.</param>
+		/// <returns>The opaque wait object.</returns>
+		public static object WaitForSeconds(float seconds) => new WaitForTimeImpl(seconds);
+
+		/// <summary>
+		/// Returns an object compatible with the <see cref="Tick"/> return value to cause the coroutine to wait for
+		/// the given time span.
+		/// </summary>
+		/// <param name="time">The time to wait the coroutine for.</param>
+		/// <returns>The opaque wait object.</returns>
+		public static object WaitForTime(TimeSpan time) => new WaitForTimeImpl((float)time.TotalSeconds);
+		#endregion // Waiting
+
+		/// <summary>
+		/// Implements the ticking logic for the coroutine. The return value controls the execution of the coroutine,
+		/// and can be any of:
+		/// <list type="bullet">
+		/// <item>
+		///		<term><c>null</c></term>
+		///		<description>Continue ticking the coroutine as normal.</description>
+		/// </item>
+		/// <item>
+		///		<term><see cref="END"/></term>
+		///		<description>End the execution of the coroutine.</description>
+		/// </item>
+		/// <item>
+		///		<term><see cref="WaitForSeconds"/> or <see cref="WaitForTime"/></term>
+		///		<description>Pause execution of the coroutine for the given amount of time.</description>
+		/// </item>
+		/// <item>
+		///		<term>Non-<c>null</c> <see cref="Coroutine"/> instance</term>
+		///		<description>Pause execution of the coroutine until the returned coroutine is complete.</description>
+		/// </item>
+		/// </list>
+		/// </summary>
+		/// <returns>A value to control the continued execution of the coroutine.</returns>
+		protected internal abstract object? Tick();
+
+		/// <summary>
+		/// Called by code external to the coroutine to stop the execution.
+		/// </summary>
+		public void Stop()
+		{
+			if (Running) OnStop();
+			Running = false;
+		}
+
+		/// <summary>
+		/// Called when the coroutine is externally stopped (such as a call to <see cref="Stop"/>).
+		/// </summary>
+		protected virtual void OnStop() { }
+
+		/// <summary>
+		/// Called when the coroutine is removed from the list of active coroutines. Resource cleanup can be done here.
+		/// </summary>
+		protected internal virtual void OnRemove() { }
+	}
+}
