507c70ffdcf9da298e748c94d1b77e70ca7ba18b
Sean Moss <moss.seank@gmail.com>
2020-09-03 13:57:44 -0400

    Initial runtime info (os, cpu, memory)

---
+++ Vega/Core/DataSize.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * Microsoft Public License (Ms-PL) - Copyright (c) 2020 Sean Moss
+ * This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
+ * the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
+ */
+
+using System;
+
+namespace Vega
+{
+	/// <summary>
+	/// A lightweight wrapper around a specific amount of data. Note that this uses the traditional binary definition 
+	/// (powers of 1024) for conversions. Negative sizes are supported.
+	/// </summary>
+	public struct DataSize : IEquatable<DataSize>
+	{
+		#region Conversion
+		/// <summary>
+		/// Number of bytes in a kilobyte.
+		/// </summary>
+		public const double KB_RATIO = 1_024.0;
+		/// <summary>
+		/// Number of bytes in a megabyte.
+		/// </summary>
+		public const double MB_RATIO = 1_048_576.0;
+		/// <summary>
+		/// Number of bytes in a gigabyte.
+		/// </summary>
+		public const double GB_RATIO = 1_073_741_842.0;
+		/// <summary>
+		/// Number of bytes in a petabyte.
+		/// </summary>
+		public const double PB_RATIO = 1_099_511_627_776.0;
+		#endregion // Conversion
+
+		/// <summary>
+		/// Represents zero bytes of data.
+		/// </summary>
+		public static readonly DataSize Zero = new();
+
+		#region Fields
+		private long _bytes;
+
+		/// <summary>
+		/// The data size in bytes.
+		/// </summary>
+		public readonly long B => _bytes;
+		/// <summary>
+		/// The data size in kilobytes.
+		/// </summary>
+		public readonly double KB => _bytes / KB_RATIO;
+		/// <summary>
+		/// The data size in megabytes.
+		/// </summary>
+		public readonly double MB => _bytes / MB_RATIO;
+		/// <summary>
+		/// The data size in gigabytes.
+		/// </summary>
+		public readonly double GB => _bytes / GB_RATIO;
+		/// <summary>
+		/// The data size in petabytes.
+		/// </summary>
+		public readonly double PB => _bytes / PB_RATIO;
+		#endregion // Fields
+
+		/// <summary>
+		/// Constructs a new size object with the given byte count.
+		/// </summary>
+		/// <param name="bytes">The data size, in bytes.</param>
+		public DataSize(long bytes) => _bytes = bytes;
+
+		#region Overrides
+		readonly bool IEquatable<DataSize>.Equals(DataSize other) => other == this;
+
+		public readonly override bool Equals(object? obj) => (obj is DataSize ds) && (ds == this);
+
+		public readonly override int GetHashCode() => (int)(_bytes & 0xFFFFFF) ^ (int)(_bytes >> 32);
+
+		public override string ToString() => $"{{{_bytes} B}}";
+		#endregion // Overrides
+
+		#region Construction
+		/// <summary>
+		/// Constructs a new size object with the given size in bytes.
+		/// </summary>
+		/// <param name="bytes">The size in bytes.</param>
+		public static DataSize FromBytes(long bytes) => new DataSize(bytes);
+		/// <summary>
+		/// Constructs a new size object with the given size in kilobytes.
+		/// </summary>
+		/// <param name="kilobytes">The size in bytes.</param>
+		public static DataSize FromKilo(double kilobytes) => new DataSize((long)(kilobytes * KB_RATIO));
+		/// <summary>
+		/// Constructs a new size object with the given size in megabytes.
+		/// </summary>
+		/// <param name="megabytes">The size in bytes.</param>
+		public static DataSize FromMega(double megabytes) => new DataSize((long)(megabytes * MB_RATIO));
+		/// <summary>
+		/// Constructs a new size object with the given size in gigabytes.
+		/// </summary>
+		/// <param name="gigabytes">The size in bytes.</param>
+		public static DataSize FromGiga(double gigabytes) => new DataSize((long)(gigabytes * GB_RATIO));
+		/// <summary>
+		/// Constructs a new size object with the given size in petabytes.
+		/// </summary>
+		/// <param name="petabytes">The size in bytes.</param>
+		public static DataSize FromPeta(double petabytes) => new DataSize((long)(petabytes * PB_RATIO));
+		#endregion // Construction
+
+		#region Operators
+		public static bool operator == (in DataSize l, in DataSize r) => l._bytes == r._bytes;
+		public static bool operator != (in DataSize l, in DataSize r) => l._bytes != r._bytes;
+		public static bool operator <  (in DataSize l, in DataSize r) => l._bytes < r._bytes;
+		public static bool operator <= (in DataSize l, in DataSize r) => l._bytes <= r._bytes;
+		public static bool operator >  (in DataSize l, in DataSize r) => l._bytes > r._bytes;
+		public static bool operator >= (in DataSize l, in DataSize r) => l._bytes >= r._bytes;
+
+		public static DataSize operator + (in DataSize l, in DataSize r) => new DataSize(l._bytes + r._bytes);
+		public static DataSize operator - (in DataSize l, in DataSize r) => new DataSize(l._bytes - r._bytes);
+		public static DataSize operator * (in DataSize l, double scale) => 
+			new DataSize((long)(l._bytes * Math.Max(0, scale)));
+		public static DataSize operator * (double scale, in DataSize r) => 
+			new DataSize((long)(r._bytes * Math.Max(0, scale)));
+		public static DataSize operator / (in DataSize l, double scale) => 
+			new DataSize((long)(l._bytes / Math.Max(0, scale)));
+		#endregion // Operators
+	}
+}
---
+++ Vega/Runtime.cs
@@ -0,0 +1,143 @@
+﻿/*
+ * Microsoft Public License (Ms-PL) - Copyright (c) 2020 Sean Moss
+ * This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
+ * the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
+ */
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Vega
+{
+	/// <summary>
+	/// Provides general runtime information about the platform and environment for the current application.
+	/// </summary>
+	public static class Runtime
+	{
+		/// <summary>
+		/// Informaton about the operating system.
+		/// </summary>
+		public static class OS
+		{
+			#region Fields
+			/// <summary>
+			/// The current operating system family.
+			/// </summary>
+			public static readonly OSFamily Family;
+
+			/// <summary>
+			/// Gets if the operating system is Microsoft Windows desktop.
+			/// </summary>
+			public static bool IsWindows => Family == OSFamily.Windows;
+			/// <summary>
+			/// Gets if the operating system is Apple Mac OSX desktop.
+			/// </summary>
+			public static bool IsMacOS => Family == OSFamily.MacOS;
+			/// <summary>
+			/// Gets if the operating system is a Linux desktop variant.
+			/// </summary>
+			public static bool IsLinuxDesktop => Family == OSFamily.LinuxDesktop;
+
+			/// <summary>
+			/// The version of the current operating system.
+			/// </summary>
+			public static readonly Version Version;
+			#endregion // Fields
+
+			static OS()
+			{
+				var osName = Environment.OSVersion.VersionString;
+				Family = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? OSFamily.Windows :
+					RuntimeInformation.IsOSPlatform(OSPlatform.macOS) ? OSFamily.MacOS :
+					RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? OSFamily.LinuxDesktop :
+					throw new InvalidOperationException($"Cannot load Vega on platform '{osName}'");
+				Version = Environment.OSVersion.Version;
+			}
+		}
+
+		/// <summary>
+		/// Information about the CPU.
+		/// </summary>
+		public static class CPU
+		{
+			#region Fields
+			/// <summary>
+			/// The number of distinct processors available to the process (either physical or hyperthreaded).
+			/// </summary>
+			public static readonly uint ProcCount;
+			/// <summary>
+			/// The processor architecture.
+			/// </summary>
+			public static CPUArch Arch;
+			#endregion // Fields
+
+			static CPU()
+			{
+				ProcCount = (uint)Environment.ProcessorCount;
+				var arch = Assembly.GetExecutingAssembly().GetName().ProcessorArchitecture;
+				Arch = arch switch { 
+					ProcessorArchitecture.Amd64 => CPUArch.X64,
+					ProcessorArchitecture.Arm => CPUArch.Arm64,
+					_ => throw new InvalidOperationException($"Unknown processor architecture ({arch})")
+				};
+			}
+		}
+
+		/// <summary>
+		/// Information about the physical memory.
+		/// </summary>
+		public static class Memory
+		{
+			#region Fields
+			/// <summary>
+			/// Gets the approximate amount of memory used by the process.
+			/// </summary>
+			public static DataSize Used => new DataSize(GC.GetTotalMemory(false));
+			/// <summary>
+			/// Gets a more accurate reading of total used memory, but may cause a blocking garbage collection.
+			/// </summary>
+			public static DataSize UsedAccurate => new DataSize(GC.GetTotalMemory(true));
+			#endregion // Fields
+
+			static Memory()
+			{
+
+			}
+		}
+	}
+
+	/// <summary>
+	/// Supported operating system families.
+	/// </summary>
+	public enum OSFamily
+	{
+		/// <summary>
+		/// Microsoft Windows desktop.
+		/// </summary>
+		Windows,
+		/// <summary>
+		/// Apple desktop Mac OSX.
+		/// </summary>
+		MacOS,
+		/// <summary>
+		/// Linux desktop variant.
+		/// </summary>
+		LinuxDesktop
+	}
+
+	/// <summary>
+	/// Supported CPU architectures.
+	/// </summary>
+	public enum CPUArch
+	{
+		/// <summary>
+		/// The AMD64 (x86_64) architecture.
+		/// </summary>
+		X64,
+		/// <summary>
+		/// The 64-bit ARM architecture.
+		/// </summary>
+		Arm64
+	}
+}
