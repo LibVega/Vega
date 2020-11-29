bdb0236b110c52235165c2f6af486a6872e0287d
Sean Moss <moss.seank@gmail.com>
2020-11-29 14:07:20 -0500

    Base type for buffers and buffer usage policies

---
+++ Vega/Graphics/Buffer/BufferBase.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Microsoft Public License (Ms-PL) - Copyright (c) 2020 Sean Moss
+ * This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
+ * the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
+ */
+
+using System;
+using Vulkan;
+
+namespace Vega.Graphics
+{
+	/// <summary>
+	/// Base type for all graphics data buffer types, providing common functionality.
+	/// </summary>
+	public unsafe abstract class BufferBase : ResourceBase
+	{
+		#region Fields
+		/// <summary>
+		/// The size of the buffer data, in bytes.
+		/// </summary>
+		public readonly ulong DataSize;
+		/// <summary>
+		/// The buffer usage policy.
+		/// </summary>
+		public readonly BufferUsage Usage;
+
+		// The vulkan buffer object
+		internal readonly VkBuffer Handle;
+		// The memory allocation object
+		internal readonly MemoryAllocation Memory;
+		#endregion // Fields
+
+		private protected BufferBase(ulong size, ResourceType type, BufferUsage usage)
+			: base(type)
+		{
+			// Set fields
+			DataSize = size;
+			Usage = usage;
+			
+			var gd = Core.Instance!.Graphics;
+
+			// Create buffer handle
+			VkBufferCreateInfo bci = new(
+				flags: VkBufferCreateFlags.NoFlags,
+				size: size,
+				usage: GetUsageFlags(type),
+				sharingMode: VkSharingMode.Exclusive
+			);
+			VulkanHandle<VkBuffer> handle;
+			gd.VkDevice.CreateBuffer(&bci, null, &handle)
+				.Throw("Failed to create buffer handle");
+			Handle = new(handle, gd.VkDevice);
+
+			// Allocate and bind buffer memory
+			VkMemoryRequirements memreq;
+			Handle.GetBufferMemoryRequirements(&memreq);
+			Memory = gd.Resources.AllocateMemoryDevice(memreq) ??
+				throw new Exception("Failed to allocate memory for buffer");
+			Handle.BindBufferMemory(Memory.Handle, Memory.Offset);
+		}
+
+		protected override void OnDispose(bool disposing)
+		{
+			// TODO: Submit to delayed destroy queue
+		}
+
+		private static VkBufferUsageFlags GetUsageFlags(ResourceType type)
+		{
+			var flags = VkBufferUsageFlags.TransferDst;
+			flags |= type switch {
+				_ => throw new ArgumentOutOfRangeException(nameof(type), "Invalid resource type")
+			};
+			return flags;
+		}
+	}
+}
---
+++ Vega/Graphics/Buffer/BufferUsage.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Microsoft Public License (Ms-PL) - Copyright (c) 2020 Sean Moss
+ * This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
+ * the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
+ */
+
+using System;
+
+namespace Vega.Graphics
+{
+	/// <summary>
+	/// The different usage policies for buffer resources. These dictate how and when buffer data can be updated.
+	/// </summary>
+	public enum BufferUsage : byte
+	{
+		/// <summary>
+		/// The buffer data is set at construction and cannot be updated at any point.
+		/// </summary>
+		Static
+	}
+}
