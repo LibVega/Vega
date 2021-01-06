c293e229f8c6f05eeb2185d9e502ae93b7dacfee
Sean Moss <moss.seank@gmail.com>
2021-01-06 11:58:14 -0500

    Update copyright year

--- Vega/Input/MouseEvent.cs
+++ Vega/Input/MouseEvent.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Microsoft Public License (Ms-PL) - Copyright (c) 2020 Sean Moss
+ * Microsoft Public License (Ms-PL) - Copyright (c) 2020-2021 Sean Moss
  * This file is subject to the terms and conditions of the Microsoft Public License, the text of which can be found in
  * the 'LICENSE' file at the root of this repository, or online at <https://opensource.org/licenses/MS-PL>.
  */
